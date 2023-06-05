fb01255ad7fbfa97d687f2cf0f281708513d7f19
[Tomas Valdez] <Tomasvaldez02@gmail.com>
2023-06-05 11:52:28 -0300

    ejercicio 1

---
+++ ejercicio1.js
@@ -0,0 +1,9 @@
+let numero = parseInt(prompt("Ingrese un numero: "));
+
+if (numero > 0) {
+  console.log("El numero es positivo");
+} else if (numero < 0) {
+  console.log("El numero es negativo");
+} else {
+  console.log("El numero es cero");
+}
